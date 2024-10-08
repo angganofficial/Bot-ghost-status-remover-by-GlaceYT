d5551588b7f924fec89e177ce187f2e92c25befe
AngganXD <107970597+angganofficial@users.noreply.github.com>
2024-10-08 18:18:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["J&T Cabang Jakarta Pusat","/cekresi || Cek Resi Kamu ( Only J&T )","Diskon Ongkir Hingga 50%", "Menerima All Paket"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
